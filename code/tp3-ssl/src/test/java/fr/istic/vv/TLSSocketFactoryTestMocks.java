8e7c615406f22054f41580e9d2e63ed8f7c99e9f
Florian Alphonzair <florian.alphonzair@etudiant.univ-rennes1.fr>
2024-01-23 23:48:19 +0100

    Write two tests with mock but they are false.

--- code/tp3-ssl/src/test/java/fr/istic/vv/TLSSocketFactoryTestMocks.java
+++ code/tp3-ssl/src/test/java/fr/istic/vv/TLSSocketFactoryTestMocks.java
@@ -10,10 +10,33 @@ import java.util.Collections;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.anySet;
+import static org.mockito.Mockito.*;
 
 public class TLSSocketFactoryTestMocks {
     @Test
-    public void test1() {
+    public void preparedSocket_NullProtocols() {
+            TLSSocketFactory f = new TLSSocketFactory();
+            SSLSocket socket = mock(SSLSocket.class);
+            when(socket.getSupportedProtocols()).thenReturn(null);
+            when(socket.getEnabledProtocols()).thenReturn(null);
+            doThrow(NullPointerException.class).when(socket).setEnabledProtocols(null);
+
+            assertThrows(NullPointerException.class, () -> f.prepareSocket(socket));
+    }
+
+    @Test
+    public void typical() {
+        String[] supportedProtocols = new String[]{"SSLv2Hello", "SSLv3", "TLSv1", "TLSv1.1", "TLSv1.2"};
+        String[] enabledProtocols = new String[]{"SSLv3", "TLSv1"};
+
+        TLSSocketFactory f = new TLSSocketFactory();
+        SSLSocket socket = mock(SSLSocket.class);
+        when(socket.getSupportedProtocols()).thenReturn(supportedProtocols);
+        when(socket.getEnabledProtocols()).thenReturn(enabledProtocols);
+        //doThrow(NullPointerException.class).when(socket).setEnabledProtocols(new String[0]);
+
+        assertArrayEquals(new String[]{"TLSv1.2", "TLSv1.1", "TLSv1", "TLS"}, socket.getEnabledProtocols());
 
     }
 }
